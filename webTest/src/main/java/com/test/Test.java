780d4057e7c713d2e82ecc0413a0a8ba32ba1fa2
Frivolity <334497582@qq.com>
2018-05-23 16:44:10 +0800

    Initial commit

---
+++ webTest/src/main/java/com/test/Test.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Test() {
+
+    }
+
+    public Test(String name) {
+        this.name = name;
+    }
+}
